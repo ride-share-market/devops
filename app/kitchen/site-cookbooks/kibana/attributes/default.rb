6eecbc0bc95bd6213f4c46620c7afccb798272fd
Rudi Starcevic <ooly.me@gmail.com>
2015-06-09 21:39:42 +0800

    Update to latest kibana

--- app/kitchen/site-cookbooks/kibana/attributes/default.rb
+++ app/kitchen/site-cookbooks/kibana/attributes/default.rb
@@ -1,9 +1,8 @@
-# default["kibana"]["src_filename"] = "kibana-4.0.0-beta3"
-default["kibana"]["src_filename"] = "kibana-4.0.0-rc1-linux-x64"
+default["kibana"]["src_filename"] = "kibana-4.0.2-linux-x64"
 
 default["kibana"]["src_filename_type"] = "tar.gz"
 
-default["kibana"]["src_url"] = "https://download.elasticsearch.org/kibana/kibana/#{default["kibana"]["src_filename"]}.#{default["kibana"]["src_filename_type"]}"
+default["kibana"]["src_url"] = "https://download.elastic.co/kibana/kibana/#{default["kibana"]["src_filename"]}.#{default["kibana"]["src_filename_type"]}"
 
 default["kibana"]["extract_path"] = "/opt/kibana"
 
