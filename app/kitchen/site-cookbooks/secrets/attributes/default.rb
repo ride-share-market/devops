8968331f0603e9396cbec2ef80692cc308833737
Rudi Starcevic <ooly.me@gmail.com>
2015-07-12 14:52:45 +0800

    Set secrets default env home to root

--- app/kitchen/site-cookbooks/secrets/attributes/default.rb
+++ app/kitchen/site-cookbooks/secrets/attributes/default.rb
@@ -1,3 +1,7 @@
+# Default env home to /root.
+# Enables this recipe to run from either chef-client (no env home) and chef-solo
+ENV['HOME'] = '/root' if ENV['HOME'] == nil
+
 default["secrets"]["chef_secret_key"] = "#{ENV['HOME']}/.ssh/chef_secret_key.txt"
 
 default["secrets"]["data"] = nil
--- app/kitchen/site-cookbooks/secrets/recipes/default.rb
+++ app/kitchen/site-cookbooks/secrets/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-# Cookbooks needing enrypted data_bags data will depend on this cookbook.
+# Cookbooks needing encrypted data_bags data will depend on this cookbook.
 raise "File Not Found: required secret key for the 'secrets' cookbook" if !File.exist?(node["secrets"]["chef_secret_key"])
 
 require 'chef/encrypted_data_bag_item'
