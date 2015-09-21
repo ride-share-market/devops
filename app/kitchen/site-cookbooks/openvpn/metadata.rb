2ba6a270c922cd9eba6859d31b2acc6497c07b32
Rudi Starcevic <ooly.me@gmail.com>
2015-09-21 22:51:35 +0800

    Update openvpn server config VPC private subnet route

--- app/kitchen/site-cookbooks/openvpn/metadata.rb
+++ app/kitchen/site-cookbooks/openvpn/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'systemsadmin@ridesharemarket.com'
 license          'All rights reserved'
 description      'Installs/Configures openvpn'
 long_description 'Installs/Configures openvpn'
-version          '1.0.1'
+version          '1.0.2'
 
 depends 'secrets'
